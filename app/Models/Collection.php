7e90a9e68a27e2f7f3d050d5161f883746bf31d6
saad <saadounmtsm@gmail.com>
2021-05-17 23:04:41 +0100

    fff

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Collection extends Model
 {
     use HasFactory;
-    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'city',
         'email',
         'phone',
+        'password',
     ];
 
     /**
