49db5725ab7f783ad7beaf6bb5d5aa8a4c4947f8
saad <saadounmtsm@gmail.com>
2021-05-17 17:57:31 +0100

    fff

--- database/migrations/2021_05_17_144820_create_collections_table.php
+++ database/migrations/2021_05_17_144820_create_collections_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateCollectionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('collections', function (Blueprint $table) {
             $table->id();
             $table->string('reference')->unique();
             $table->string('name');
@@ -30,6 +30,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('collections');
     }
 }
